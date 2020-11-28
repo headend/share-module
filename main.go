7f273d57376ae89b89e4254ba4f3e0f4871bc31d
thuonghh <thuonghh@vng.com.vn>
2020-11-28 18:03:25 +0700

    debug share model

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"github.com/headend/share-module/model"
+)
+
 func main()  {
+	var test model.WorkerUpdateSignal
+	test.FilePath = "ss"
+	fmt.Printf("%#v" ,test)
 	print("Shared module\n")
 }
--- model/gateway-agentd.go
+++ model/gateway-agentd.go
@@ -4,5 +4,6 @@ type WorkerUpdateSignal struct {
 	FileName	string	`json:"fileName"`
 	FilePath	string	`json:"filePath"`
 	FileSizeInByte	int64	`json:"fileSizeInByte"`
-	Md5	string	`json:"md5"`}
+	Md5	string	`json:"md5"`
+}
 
