f9c183a0e125cfdfbc763cbe98c24212bd811b57
thuonghh <thuonghh@vng.com.vn>
2020-11-28 21:52:14 +0700

    static config - user can not modify

--- configuration/static-config/static-config.go
+++ configuration/static-config/static-config.go
@@ -1,1 +1,15 @@
 package static_config
+
+const (
+	GatewayHost = "0.0.0.0"
+	GatewayPort = 8000
+	GatewayStorage = "asset/"
+	MasterHost = "127.0.0.1"
+	MasterPort = 45656
+	InstallationPath = "/opt/iptv"
+	LogPath = "/opt/iptv/log"
+	BinaryPath = "/opt/iptv/sbin"
+	ConfigFilePath = "/opt/iptv/application.yml"
+	VersionFilePath = "/opt/iptv/version"
+)
+
