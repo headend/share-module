be490e72949e255d877c7a540df057f4593174c1
thuonghh <thuonghh@vng.com.vn>
2020-11-28 21:08:29 +0700

    declare socket event

---
+++ configuration/socket-event/socket-event.go
@@ -0,0 +1,17 @@
+package socket_event
+
+// topic declare
+const (
+	// event to receive notice
+	thongbao  = "notice"
+	// event to recieve error
+	loi = "error"
+	// event to receive connection message
+	ketnoi = "connection"
+	// event to handle disconnect event
+	ngatketnoi = "disconnection"
+	// event recieve file
+	nhanfile = "receivefile"
+	// event recieve log
+	nhanlog = "log"
+)
