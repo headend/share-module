db46dd2e31adb1ba576e1ecb71e86319b86acd54
thuonghh <thuonghh@vng.com.vn>
2020-11-28 17:42:24 +0700

    Worker update message struct

---
+++ model/gateway-agentd.go
@@ -0,0 +1,8 @@
+package model
+
+type WorkerUpdateSignal struct {
+	FileName	string	`json:"fileName"`
+	FilePath	string	`json:"filePath"`
+	FileSizeInByte	int64	`json:"fileSizeInByte"`
+	Md5	string	`json:"md5"`
+}
