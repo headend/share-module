e8518b5083c00a163b231885f423605c691eb80f
thuonghh <thuonghh@vng.com.vn>
2020-11-28 21:08:53 +0700

    Golang execute command shell

--- shellout/shellout.go
+++ shellout/shellout.go
@@ -1,1 +1,16 @@
 package shellout
+
+import (
+	"bytes"
+	"os/exec"
+)
+
+func ExecuteCommand(shell string, command string) (error, string, string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(shell, "-c", command)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	return err, stdout.String(), stderr.String()
+}
