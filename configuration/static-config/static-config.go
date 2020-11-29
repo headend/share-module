87c75c40bb596e9f53d1fb73c12c2f58755444cc
thuonghh <thuonghh@vng.com.vn>
2020-11-29 17:27:13 +0700

    static config for master

--- configuration/static-config/static-config.go
+++ configuration/static-config/static-config.go
@@ -4,8 +4,15 @@ const (
 	GatewayHost = "0.0.0.0"
 	GatewayPort = 8000
 	GatewayStorage = "asset/"
+	GatewayUser = "user"
+	GatewayPassword = "pass"
+	GatewayTransportProtocol = "websocket"
 	MasterHost = "127.0.0.1"
 	MasterPort = 45656
+	MasterStorage = "asset/"
+	MasterUser = "user"
+	MasterPassword = "pass"
+	MasterTransportProtocol = "websocket"
 	InstallationPath = "/opt/iptv"
 	LogPath = "/opt/iptv/log"
 	BinaryPath = "/opt/iptv/sbin"
