18d49ae4171aa8e02c5b08db24d1c3ca2b929594
thuonghh <thuonghh@vng.com.vn>
2020-11-30 07:40:16 +0700

    module xử lý DB - Mysql

---
+++ databases/mysql.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"source-centertower.mto.zing.vn/micro-services/manage-modules/configuration"
+	"time"
+)
+
+type Database struct {
+	Db *gorm.DB
+	Err error
+}
+
+func (e *Database) Connect(config *configuration.Conf) *Database {
+	config.LoadConf()
+	args := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", config.DB.User, config.DB.Password, config.DB.Host, config.DB.Port, config.DB.DBName)
+	//log.Printf("Connection info: %v", args)
+	e.Db, e.Err = gorm.Open("mysql", args)
+	e.Db.DB().SetConnMaxLifetime(time.Minute * 5)
+	e.Db.DB().SetMaxIdleConns(0)
+	e.Db.DB().SetMaxOpenConns(10000)
+	return e
+}
+
