206b162c2526fae0c2ac664c9781b3dca71ec6f6
thuonghh <thuonghh@vng.com.vn>
2020-12-15 20:28:17 +0700

    define message communiction between frontend vaf backend

--- model/gateway-api.go
+++ model/gateway-api.go
@@ -1,1 +1,21 @@
 package model
+
+type AgentCtlRequest struct {
+	AgentIp	string	`json:"agent_ip"`
+	ActionType	int	`json:"action_type"`
+	ActionId	int	`json:"action_id"`
+	TunnelData	map[string]string	`json:"tunnel_data"`
+}
+
+type AgentCtlResponse struct {
+	ReturnCode	string `json:"return_code"`
+	ReturnData	AgentCtlDataResponse	`json:"return_data"`
+	ReturnMessage	string	`json:"return_message"`
+	TunnelData	map[string]string	`json:"tunnel_data"`
+}
+
+type AgentCtlDataResponse struct {
+	ActionId	int	`json:"action_id"`
+	ActionType	int	`json:"action_type"`
+}
+
