b2bab52a7224f74fcd9217603ed9cb538bad6e9e
thuonghh <thuonghh@vng.com.vn>
2020-12-15 21:24:22 +0700

    server config add request timeout config

--- configuration/models.go
+++ configuration/models.go
@@ -42,18 +42,19 @@ type Conf struct {
 		User     string `yaml:"user"`
 		Password string `yaml:"password"`
 		DBName   string `yaml:"dbname"`
-	}
+	}	`yaml:"db"`
 	Server struct {
 		Port    uint16 `yaml:"port"`
 		Host    string `yaml:"host"`
 		Gateway string `yaml:"gateway"`
-	}
+		RequestTimeout	int	`yaml:"request_timeout"`
+	}	`yaml:"server"`
 
 	RPC struct {
-		Agent          Agent
-		Agentctl       Agentctl
-		Agentrunner    Agentexecuter
-	}
+		Agent          Agent	`yaml:"agent"`
+		Agentctl       Agentctl	`yaml:"agentctl"`
+		Agentrunner    Agentexecuter	`yaml:"agentrunner"`
+	}	`yaml:"rpc"`
 	Warmup struct {
 		Concurrency       int `yaml:"concurrency"`
 		MigrationInterval int `yaml:"migration_interval"`
@@ -61,11 +62,11 @@ type Conf struct {
 
 	Nra struct {
 		Concurrency int `yaml:"concurrency"`
-	}
+	}	`yaml:"nra"`
 
 	Redis struct {
 		Host         string `yaml:"host"`
-		Port         uint16    `yaml:"port"`
+		Port         uint16	`yaml:"port"`
 		DB           int    `yaml:"db"`
 		Password     string `yaml:"password"`
 		Key          string `yaml:"key"`
@@ -74,10 +75,10 @@ type Conf struct {
 		WriteTimeout int32  `yaml:"write_timeout"`
 		PoolSize     int    `yaml:"pool_size"`
 		PoolTimeout  int32  `yaml:"pool_timeout"`
-	}
+	}	`yaml:"redis"`
 	MQ struct {
 		Host                     string `yaml:"host"`
-		Port                     uint16    `yaml:"port"`
+		Port                     uint16	`yaml:"port"`
 		Topic                    string `yaml:"topic"`
 		WarmUpTopic              string `yaml:"warmup_topic"`
 		NraTopic                 string `yaml:"nra_topic"`
@@ -85,18 +86,18 @@ type Conf struct {
 		CommandTopic             string `yaml:"command_topic"`
 		Username                 string `yaml:"Username"`
 		Password                 string `yaml:"Password"`
-	}
+	}	`yaml:"mq"`
 
 	AgentGateway struct{
 		Host			string `yaml:"host"`
 		Port            uint16 `yaml:"port"`
 		Gateway			string `yaml:"gateway"`
 		Storage			string `yaml:"storage"`
-	}
+	} `yaml:"agent_gateway"`
 
 	Agentd struct {
 		Master			AgentMaster	`yaml:"master"`
 		Worker			AgentWorker `yaml:"worker"`
-	}
+	} `yaml:"agentd"`
 
 }
