3cca4aa2a3f763029ca844e7003c7981797df85b
hhthuongbtr <hhthuongbtr@gmail.com>
2020-12-17 00:20:36 +0700

    Update gateway-api.go

--- model/gateway-api.go
+++ model/gateway-api.go
@@ -6,28 +6,6 @@ type AgentCtlRequest struct {
 	 */
 	AgentIp	string	`json:"agent_ip"`
 	/*
-	Control type, define from backend
-			// start agent
-			StartAgentd = 10000
-			// stop agent
-			StopAgentd = 10001
-			// Connect agent
-			ConnectAgentd = 10030
-			// Disconnect agent
-			DisconnectAgentd = 10031
-			// Start Worker
-			StartWorker = 20000
-			// Stop worker
-			StopWorker = 20001
-			//Update Worker
-			UpdateWorker = 20010
-			Signal = 70001
-			Video = 70002
-			Audio = 70003
-			BlackScreen = 70004
-	 */
-	ControlType	int	`json:"control_type"`
-	/*
 		Control ID from frontend
 	*/
 	ControlId	int	`json:"control_id"`
@@ -59,28 +37,6 @@ type AgentCtlResponse struct {
 }
 
 type AgentCtlDataResponse struct {
-	/*
-		Control type, define from backend
-				// start agent
-				StartAgentd = 10000
-				// stop agent
-				StopAgentd = 10001
-				// Connect agent
-				ConnectAgentd = 10030
-				// Disconnect agent
-				DisconnectAgentd = 10031
-				// Start Worker
-				StartWorker = 20000
-				// Stop worker
-				StopWorker = 20001
-				//Update Worker
-				UpdateWorker = 20010
-				Signal = 70001
-				Video = 70002
-				Audio = 70003
-				BlackScreen = 70004
-	*/
-	ControlType	int	`json:"control_type"`
 	/*
 		Control ID from frontend
 	*/
