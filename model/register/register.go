816b53ecf30135decc8c2219f77680c972f2515e
thuonghh <thuonghh@vng.com.vn>
2020-12-20 23:07:49 +0700

    add register agent

---
+++ model/register/register.go
@@ -0,0 +1,10 @@
+package register
+
+type Register struct {
+	IP	string	`json:"ip"`
+	Port	string `json:"port"`
+	Cpu	int	`json:"cpu"`
+	Ram int `json:"ram"`
+	Disk int `json:"disk"`
+	EventTime int64 `json:"event_time"`
+}
