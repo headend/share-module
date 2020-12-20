7ccbd25afd8d83848117844fb71e51d5c03dd95a
thuonghh <thuonghh@vng.com.vn>
2020-12-20 23:38:56 +0700

    add method convert to json

--- model/register/register.go
+++ model/register/register.go
@@ -1,5 +1,7 @@
 package register
 
+import "encoding/json"
+
 type Register struct {
 	IP		string	`json:"ip"`
 	Port	string `json:"port"`
@@ -7,4 +9,20 @@ type Register struct {
 	Ram		int `json:"ram"`
 	Disk	int `json:"disk"`
 	EventTime int64 `json:"event_time"`
+}
+
+func (NRA *Register) LoadFromJsonString(JsonString string) (err error) {
+	err = json.Unmarshal([]byte(JsonString), NRA)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+func (NRA *Register) GetJsonString() (JsonString string, err error) {
+	b, err := json.Marshal(NRA)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
 }
--- model/warmup/warmup.go
+++ model/warmup/warmup.go
@@ -1,5 +1,7 @@
 package warmup
 
+import "encoding/json"
+
 type WarmupMessage struct {
 	EventTime	int64 `json:"event_time"`
 	Data	[]WarmupElement `json:"data"`
@@ -11,4 +13,36 @@ type WarmupElement struct {
 	Cpu	int	`json:"cpu"`
 	Ram int `json:"ram"`
 	Disk int `json:"disk"`
+}
+
+func (WM *WarmupMessage) GetJsonString() (JsonString string, err error) {
+	b, err := json.Marshal(WM)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func (WM *WarmupMessage) LoadFromJsonString(JsonString string) (err error) {
+	err = json.Unmarshal([]byte(JsonString), WM)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+func (WE *WarmupElement) GetJsonString() (JsonString string, err error) {
+	b, err := json.Marshal(WE)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func (WE *WarmupElement) LoadFromJsonString(JsonString string) (err error) {
+	err = json.Unmarshal([]byte(JsonString), WE)
+	if err != nil {
+		return err
+	}
+	return
 }
