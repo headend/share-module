3563da216f2b21fee7088f7855da48cf13a8b711
thuonghh <thuonghh@vng.com.vn>
2020-12-20 20:48:04 +0700

    define message communication to agentd

---
+++ model/agentd-worker.go
@@ -0,0 +1,74 @@
+package model
+
+import "encoding/json"
+
+type ProfileForAgentd struct {
+	ProfileId	int64	`json:"profile_id"`
+	AgentId		int64	`json:"agent_id"`
+	Status		int		`json:"status"`
+	VideoStatus	bool	`json:"video_status"`
+	AudioStatus bool	`json:"audio_status"`
+	MulticastIP string	`json:"multicast_ip"`
+}
+
+type MonitorInputForAgent struct {
+	MonitorType	int	`json:"monitor_type"`
+	ProfileList []ProfileForAgentd `json:"profile_list"`
+}
+
+func (PFA *ProfileForAgentd) GetJsonString() (JsonString string, err error) {
+	b, err := json.Marshal(PFA)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func (PFA *ProfileForAgentd) LoadFromJsonString(JsonString string) (err error) {
+	err = json.Unmarshal([]byte(JsonString), PFA)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+func (MIFA *MonitorInputForAgent) GetJsonString() (JsonString string, err error) {
+	b, err := json.Marshal(MIFA)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func (MIFA *MonitorInputForAgent) LoadFromJsonString(JsonString string) (err error) {
+	err = json.Unmarshal([]byte(JsonString), MIFA)
+	if err != nil {
+		return err
+	}
+	return
+}
+
+type ProfileChangeStatus struct {
+	ProfileId	int64	`json:"profile_id"`
+	AgentId		int64	`json:"agent_id"`
+	OldStatus		int64		`json:"old_status"`
+	NewStatus	int64	`json:"new_status"`
+	EventTime int64	`json:"event_time"`
+}
+
+func (PCS *ProfileChangeStatus) GetJsonString() (JsonString string, err error) {
+	b, err := json.Marshal(PCS)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func (PCS *ProfileChangeStatus) LoadFromJsonString(JsonString string) (err error) {
+	err = json.Unmarshal([]byte(JsonString), PCS)
+	if err != nil {
+		return err
+	}
+	return
+}
+
