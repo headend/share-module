3f397c07bfcd7e5b861a8f15a4029cce4849d733
thuonghh <thuonghh@vng.com.vn>
2020-12-20 23:44:36 +0700

    add rpcclient module

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/confluentinc/confluent-kafka-go v1.5.2
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	google.golang.org/grpc v1.34.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
---
+++ mygrpc/connection/init-connection.go
@@ -0,0 +1,23 @@
+package connection
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+)
+
+type RpcClient struct {
+	Client *grpc.ClientConn
+	Err error
+}
+
+func (rpc_cli *RpcClient) InitializeClient(host string, port string) *RpcClient {
+	rpcServerAddr := fmt.Sprintf("%s:%d", host, port)
+	rpc_cli.Client, rpc_cli.Err = grpc.Dial(rpcServerAddr,
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(20 * 1024 * 1024)),
+		grpc.WithInsecure())
+	if rpc_cli.Err != nil {
+		log.Fatalf("failed to connect server: %v", rpc_cli.Err)
+	}
+	return rpc_cli
+}
