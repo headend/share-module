50e2ea30e4b8917f7872f4f994488e4db7e08a53
thuonghh <thuonghh@vng.com.vn>
2020-12-25 08:04:04 +0700

    add monitorid field to ProfileForAgentdElement

--- model/agentd/agentd-worker.go
+++ model/agentd/agentd-worker.go
@@ -3,9 +3,10 @@ package model
 import "encoding/json"
 
 type ProfileForAgentdElement struct {
+	MonitorId	int64	`json:"monitor_id"`
 	ProfileId	int64	`json:"profile_id"`
 	AgentId		int64	`json:"agent_id"`
-	Status		int		`json:"status"`
+	Status		int64		`json:"status"`
 	VideoStatus	bool	`json:"video_status"`
 	AudioStatus bool	`json:"audio_status"`
 	MulticastIP string	`json:"multicast_ip"`
