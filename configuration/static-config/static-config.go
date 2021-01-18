c7b294a7eab55011b9b95f7cab2c9ee7e51c0224
thuonghh <thuonghh@vng.com.vn>
2021-01-18 23:06:30 +0700

    gateway storage

--- configuration/static-config/static-config.go
+++ configuration/static-config/static-config.go
@@ -3,13 +3,13 @@ package static_config
 const (
 	GatewayHost = "0.0.0.0"
 	GatewayPort = 8000
-	GatewayStorage = "asset/"
+	GatewayStorage = "/opt/iptv/asset"
 	GatewayUser = "user"
 	GatewayPassword = "pass"
 	GatewayTransportProtocol = "websocket"
 	MasterHost = "127.0.0.1"
 	MasterPort = 45656
-	MasterStorage = "asset/"
+	MasterStorage = "/opt/iptv/asset"
 	MasterUser = "user"
 	MasterPassword = "pass"
 	MasterTransportProtocol = "websocket"
